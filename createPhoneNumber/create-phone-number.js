c1da818ed6091e4e42f6f88f6b97da57e80cbc55
TKadyear <tamarakadyear@gmail.com>
2022-01-29 14:40:37 +0100

    First test Done

--- createPhoneNumber/create-phone-number.js
+++ createPhoneNumber/create-phone-number.js
@@ -1,5 +1,5 @@
-function createPhoneNumber() {
-  let phone = ''
+function createPhoneNumber(array) {
+  let phone = `(${[...array].slice(0, 3).join('')}) ${[...array].slice(3, 6).join('')}-${[...array].slice(6, 10).join('')}`
   return phone
 }
 module.exports = createPhoneNumber;
