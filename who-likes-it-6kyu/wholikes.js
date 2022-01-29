f09c029a0cf6c93d926fd0db8e8b9e943cba94d6
TKadyear <tamarakadyear@gmail.com>
2022-01-29 11:43:34 +0100

    Test and Functions kyu 6 Done -Green

--- who-likes-it-6kyu/wholikes.js
+++ who-likes-it-6kyu/wholikes.js
@@ -1,4 +1,19 @@
-function sum(a, b) {
-  return a + b;
+function whoLikes(list) {
+  const NumberPersons = list.length
+  let response = '';
+  if (NumberPersons === 0) {
+    response = 'No one like this';
+  } else if (NumberPersons === 1) {
+    response = `${list[0]} likes this`;
+  } else if (NumberPersons === 2) {
+    response = `${list[0]} and ${list[1]} likes this`;
+  } else if (NumberPersons === 3) {
+    response = `${list[0]}, ${list[1]} and ${list[2]} likes this`;
+  } else if (NumberPersons > 3) {
+
+    response += `${list[0]}, ${list[1]} and ${NumberPersons - 2} others likes this`
+
+  }
+  return response
 }
-module.exports = sum;
+module.exports = whoLikes;
--- who-likes-it-6kyu/wholikes.test.js
+++ who-likes-it-6kyu/wholikes.test.js
@@ -1,5 +1,20 @@
-const sum = require('./wholikes');
+const whoLikes = require('./wholikes');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('minMaxArray', () => {
+  test('If no one likes this', () => {
+    expect(whoLikes([])).toBe("No one like this");
+  });
+  test('If someone likes this', () => {
+    expect(whoLikes(['Gema'])).toBe("Gema likes this");
+  });
+  test('If 2 persons likes this', () => {
+    expect(whoLikes(['Gema', 'Sofía'])).toBe("Gema and Sofía likes this");
+  });
+  test('If 3 persons likes this', () => {
+    expect(whoLikes(['Gema', 'Sofía', 'Lucía'])).toBe("Gema, Sofía and Lucía likes this");
+  });
+  test('If more than 4 persons likes this', () => {
+    const listPersonLikes = ['Gema', 'Sofía', 'Lucía', 'David'];
+    expect(whoLikes(listPersonLikes)).toBe(`${listPersonLikes[0]}, ${listPersonLikes[1]} and ${listPersonLikes.length - 2} others likes this`);
+  });
 });
