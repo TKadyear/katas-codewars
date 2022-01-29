f443e7b6f834d928c28a6de03c6beb5f87e23410
TKadyear <tamarakadyear@gmail.com>
2022-01-29 12:04:57 +0100

    Corrected typos mistakes

--- who-likes-it-6kyu/wholikes.js
+++ who-likes-it-6kyu/wholikes.js
@@ -2,17 +2,15 @@ function whoLikes(list) {
   const NumberPersons = list.length
   let response = '';
   if (NumberPersons === 0) {
-    response = 'No one like this';
+    response = 'no one likes this';
   } else if (NumberPersons === 1) {
     response = `${list[0]} likes this`;
   } else if (NumberPersons === 2) {
-    response = `${list[0]} and ${list[1]} likes this`;
+    response = `${list[0]} and ${list[1]} like this`;
   } else if (NumberPersons === 3) {
-    response = `${list[0]}, ${list[1]} and ${list[2]} likes this`;
+    response = `${list[0]}, ${list[1]} and ${list[2]} like this`;
   } else if (NumberPersons > 3) {
-
-    response += `${list[0]}, ${list[1]} and ${NumberPersons - 2} others likes this`
-
+    response += `${list[0]}, ${list[1]} and ${NumberPersons - 2} others like this`
   }
   return response
 }
--- who-likes-it-6kyu/wholikes.test.js
+++ who-likes-it-6kyu/wholikes.test.js
@@ -2,19 +2,19 @@ const whoLikes = require('./wholikes');
 
 describe('minMaxArray', () => {
   test('If no one likes this', () => {
-    expect(whoLikes([])).toBe("No one like this");
+    expect(whoLikes([])).toBe("no one likes this");
   });
   test('If someone likes this', () => {
     expect(whoLikes(['Gema'])).toBe("Gema likes this");
   });
   test('If 2 persons likes this', () => {
-    expect(whoLikes(['Gema', 'Sofía'])).toBe("Gema and Sofía likes this");
+    expect(whoLikes(['Gema', 'Sofía'])).toBe("Gema and Sofía like this");
   });
   test('If 3 persons likes this', () => {
-    expect(whoLikes(['Gema', 'Sofía', 'Lucía'])).toBe("Gema, Sofía and Lucía likes this");
+    expect(whoLikes(['Gema', 'Sofía', 'Lucía'])).toBe("Gema, Sofía and Lucía like this");
   });
   test('If more than 4 persons likes this', () => {
     const listPersonLikes = ['Gema', 'Sofía', 'Lucía', 'David'];
-    expect(whoLikes(listPersonLikes)).toBe(`${listPersonLikes[0]}, ${listPersonLikes[1]} and ${listPersonLikes.length - 2} others likes this`);
+    expect(whoLikes(listPersonLikes)).toBe(`${listPersonLikes[0]}, ${listPersonLikes[1]} and ${listPersonLikes.length - 2} others like this`);
   });
 });
