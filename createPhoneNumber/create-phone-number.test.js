054cda077dd2dcbda6b657e86f5ef05421ed8017
TKadyear <tamarakadyear@gmail.com>
2022-01-29 14:20:38 +0100

    Added a new Kata and Done Test

---
+++ createPhoneNumber/create-phone-number.test.js
@@ -0,0 +1,15 @@
+const createPhoneNumber = require('./create-phone-number')
+describe('Phone Number', () => {
+  test('List of nine Numbers', () => {
+    expect(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])).toEqual('(123) 456-7890');
+  })
+  test('If some of the numbers is bigger than 9', () => {
+    expect(createPhoneNumber([1, 2, 3, 40, 5, 6, 7, 8, 9, 0])).toEqual('Invalid Phone Number');;
+  })
+  test('If the list has a String or other format', () => {
+    expect(createPhoneNumber([1, 't', 3, 4, 5, 6, 7, 8, 9, 0])).toEqual('Invalid Phone Number');
+    expect(createPhoneNumber([1, function () { }, 3, 4, 5, 6, 7, 8, 9, 0])).toEqual('Invalid Phone Number');
+    expect(createPhoneNumber([1, {}, 3, 4, 5, 6, 7, 8, 9, 0])).toEqual('Invalid Phone Number');
+    expect(createPhoneNumber([1, [], 3, 4, 5, 6, 7, 8, 9, 0])).toEqual('Invalid Phone Number');
+  })
+})
