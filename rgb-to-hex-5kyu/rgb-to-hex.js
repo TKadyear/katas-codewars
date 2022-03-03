e4161da7eb3e69e938ba5bdee9cc67ab3c2117ee
TKadyear <tamarakadyear@gmail.com>
2022-03-03 13:26:34 +0100

    Resolved kata rgb to hex

--- rgb-to-hex-5kyu/rgb-to-hex.js
+++ rgb-to-hex-5kyu/rgb-to-hex.js
@@ -1,4 +1,10 @@
 function hexConversion(R, G, B) {
-  return;
+  const HEX = "0123456789ABCDEF"
+  let conversionHEX = "";
+  [R, G, B].forEach(value => conversionHEX += (value >= 255)
+    ? "FF"
+    : HEX[Math.floor(value / 16)] + value % 16
+  )
+  return conversionHEX;
 }
 module.exports = { hexConversion }
