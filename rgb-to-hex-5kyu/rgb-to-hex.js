35450994e866df1ff382f0941b5160f1cf1cbf9a
TKadyear <tamarakadyear@gmail.com>
2022-03-03 13:53:58 +0100

    New solution rgb to hex

--- rgb-to-hex-5kyu/rgb-to-hex.js
+++ rgb-to-hex-5kyu/rgb-to-hex.js
@@ -1,10 +1,14 @@
 function hexConversion(R, G, B) {
   const HEX = "0123456789ABCDEF"
   let conversionHEX = "";
-  [R, G, B].forEach(value => conversionHEX += (value >= 255)
-    ? "FF"
-    : HEX[Math.floor(value / 16)] + value % 16
-  )
+  [R, G, B].forEach(value => {
+    if (value > 255 || value < 0) {
+      value = (value > 255) ? 255 : 0;
+    }
+    conversionHEX += (value === 255)
+      ? "FF"
+      : HEX[Math.floor(value / 16)] + HEX[Math.floor(value % 16)];
+  })
   return conversionHEX;
 }
 module.exports = { hexConversion }
