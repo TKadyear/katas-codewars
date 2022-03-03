b7d5f7b1fa6d19015ae616c8df174418c9a2082c
TKadyear <tamarakadyear@gmail.com>
2022-03-03 10:45:27 +0100

    Done Kata Create Phone Number

--- createPhoneNumber/create-phone-number.js
+++ createPhoneNumber/create-phone-number.js
@@ -1,19 +1,6 @@
 function createPhoneNumber(numbers) {
   //(123) 423-123
-  if (numbers.includes(isNaN())) {
-    return 'Invalid Phone Number'
-  }
-  if (numbers.length > 10) {
-    return 'Phone Number must have only 9 digits'
-  }
-  const isAbove10 = (value) => value < 10
-  if (numbers.every(isAbove10) && numbers.length === 10) {
-    let phone = `(${[...numbers].slice(0, 3).join('')}) ${[...numbers].slice(3, 6).join('')}-${[...numbers].slice(6, 10).join('')}`
-    return phone
-  }
-  // else {
-  //   return 'Phone Number must have only 9 digits'
-  // }
+  let phone = `(${[...numbers].slice(0, 3).join('')}) ${[...numbers].slice(3, 6).join('')}-${[...numbers].slice(6, 10).join('')}`
+  return phone
 }
-createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
 module.exports = createPhoneNumber;
--- createPhoneNumber/create-phone-number.test.js
+++ createPhoneNumber/create-phone-number.test.js
@@ -3,21 +3,4 @@ describe('Phone Number', () => {
   test('List of nine Numbers', () => {
     expect(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])).toEqual('(123) 456-7890');
   })
-  test('If length of the list numbers is bigger than 9', () => {
-    expect(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])).toEqual('Phone Number must have only 9 digits');;
-  })
-  test('If some of the numbers is bigger than 9', () => {
-    expect(createPhoneNumber([1, 2, 3, 40, 5, 6, 7, 8, 9, 0])).toEqual('Phone Number must have only 9 digits');;
-  })
-  test('If the list has a NaN', () => {
-    expect(createPhoneNumber([1, function () { }, 3, 4, 5, 6, 7, 8, 9, 0])).toEqual('Invalid Phone Number');
-    expect(createPhoneNumber([1, {}, 3, 4, 5, 6, 7, 8, 9, 0])).toEqual('Invalid Phone Number');
-    expect(createPhoneNumber([1, [], 3, 4, 5, 6, 7, 8, 9, 0])).toEqual('Invalid Phone Number');
-  })
-  test('If the list has a String or other format', () => {
-    expect(createPhoneNumber([1, 't', 3, 4, 5, 6, 7, 8, 9, 0])).toEqual('Invalid Phone Number');
-  })
-  test('If the list has a Boolean', () => {
-    expect(createPhoneNumber([1, true, 3, 4, 5, 6, 7, 8, 9, 0])).toEqual('Invalid Phone Number');
-  })
 })
