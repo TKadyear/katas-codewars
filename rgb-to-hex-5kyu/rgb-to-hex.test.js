990f7f8163621067ad228737d81654c2dab7190c
TKadyear <tamarakadyear@gmail.com>
2022-03-03 13:33:24 +0100

    Added a new test in rgb to hex
    Because the test doesn't take into account all the posible cases

--- rgb-to-hex-5kyu/rgb-to-hex.test.js
+++ rgb-to-hex-5kyu/rgb-to-hex.test.js
@@ -10,9 +10,15 @@ describe("RGB color in range 0 to 255", () => {
   test('case 3', () => {
     expect(hexConversion(148, 0, 211)).toEqual("9400D3")
   })
+  test('case 3', () => {
+    expect(hexConversion(173, 255, 47)).toEqual("ADFF2F")
+  })
 })
-describe("RGB color more than 255", () => {
-  test('case 1', () => {
+describe("RGB color more than 255 or less than 0", () => {
+  test('More than 255', () => {
     expect(hexConversion(255, 255, 300)).toEqual("FFFFFF")
+  })
+  test('Less than 0', () => {
+    expect(hexConversion(0, 0, -20)).toEqual("000000")
   })
 })
