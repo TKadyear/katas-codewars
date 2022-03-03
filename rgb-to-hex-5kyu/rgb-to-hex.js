858212c3fad6cd64003ca30dbd87fec90c09f05d
TKadyear <tamarakadyear@gmail.com>
2022-03-03 14:04:46 +0100

    Refactorized rgb to hex

--- rgb-to-hex-5kyu/rgb-to-hex.js
+++ rgb-to-hex-5kyu/rgb-to-hex.js
@@ -2,12 +2,13 @@ function hexConversion(R, G, B) {
   const HEX = "0123456789ABCDEF"
   let conversionHEX = "";
   [R, G, B].forEach(value => {
+    const index = (isFirstDigit = true) => Math.floor(isFirstDigit ? value / 16 : value % 16)
     if (value > 255 || value < 0) {
       value = (value > 255) ? 255 : 0;
     }
     conversionHEX += (value === 255)
       ? "FF"
-      : HEX[Math.floor(value / 16)] + HEX[Math.floor(value % 16)];
+      : HEX[index()] + HEX[index(false)];
   })
   return conversionHEX;
 }
