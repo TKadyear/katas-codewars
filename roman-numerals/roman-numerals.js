7df2db8f5525051a5c66bf251e80a8b5fdb51110
TKadyear <tamarakadyear@gmail.com>
2022-03-08 12:00:04 +0100

    roman-numerals

---
+++ roman-numerals/roman-numerals.js
@@ -0,0 +1,43 @@
+function divideInPartsNumber(number) {
+  number = number.toString();
+  let partNum = [];
+  for (let i = 0; i < number.length; i++) {
+    partNum.push(number[i].padEnd(number.length - i, 0))
+  }
+  return partNum;
+}
+function changeToRomanNumerals(num) {
+  const romanNumerals = {
+    1: { "1": "I", "5": "V" },
+    2: { "1": "X", "5": "L" },
+    3: { "1": "C", "5": "D" },
+    4: { "1": "M" },
+  }
+  let NumberDivided = divideInPartsNumber(num);
+  let roman = "";
+  NumberDivided.forEach((number) => {
+    let NUMBER = Number(number[0])
+    if (NUMBER === 0) {
+      return;
+    }
+    if (NUMBER != 5 && NUMBER != 1) {
+      if (NUMBER != 4 && NUMBER != 9) {
+        if (NUMBER > 5) {
+          roman += romanNumerals[number.length]["5"]
+          NUMBER = NUMBER - 5
+        }
+        for (let i = 0; i < NUMBER; i++) {
+          roman += romanNumerals[number.length]["1"]
+        }
+      } else {
+        const firstDigit = ((NUMBER + 1).toString())[0]
+        const length = NUMBER === 4 ? number.length : number.length + 1;
+        roman = roman + romanNumerals[number.length]["1"] + romanNumerals[length][firstDigit];
+      }
+    } else {
+      roman += romanNumerals[number.length][number[0]]
+    }
+  });
+  return roman;
+}
+module.exports = changeToRomanNumerals
---
+++ roman-numerals/roman-numerals.test.js
@@ -0,0 +1,71 @@
+const changeToRomanNumerals = require('./roman-numerals')
+describe('Roman-numerals', () => {
+  test('One', () => {
+    expect(changeToRomanNumerals(1)).toBe('I');
+  })
+  test('Two', () => {
+    expect(changeToRomanNumerals(2)).toBe('II');
+  })
+  test('Three', () => {
+    expect(changeToRomanNumerals(3)).toBe('III');
+  })
+  test('Four', () => {
+    expect(changeToRomanNumerals(4)).toBe('IV');
+  })
+  test('Five', () => {
+    expect(changeToRomanNumerals(5)).toBe('V');
+  })
+  test('Six', () => {
+    expect(changeToRomanNumerals(6)).toBe('VI');
+  })
+  test('Seven', () => {
+    expect(changeToRomanNumerals(7)).toBe('VII');
+  })
+  test('Eight', () => {
+    expect(changeToRomanNumerals(8)).toBe('VIII');
+  })
+  test('Nine', () => {
+    expect(changeToRomanNumerals(9)).toBe('IX');
+  })
+  test('Ten', () => {
+    expect(changeToRomanNumerals(10)).toBe('X');
+  })
+})
+
+describe('Larger numbers to 11 to 50', () => {
+  test('Ten', () => {
+    expect(changeToRomanNumerals(11)).toBe('XI');
+  })
+  test('Fity', () => {
+    expect(changeToRomanNumerals(50)).toBe('L');
+  })
+
+})
+describe('Larger numbers to 50 to 500', () => {
+  test('One Hundred ', () => {
+    expect(changeToRomanNumerals(100)).toBe('C');
+  })
+  test('Five Hundred', () => {
+    expect(changeToRomanNumerals(500)).toBe('D');
+  })
+  test('One Thousand', () => {
+    expect(changeToRomanNumerals(1000)).toBe('M');
+  })
+})
+describe('Complex Larger numbers', () => {
+  test('899', () => {
+    expect(changeToRomanNumerals(899)).toBe('DCCCXCIX');
+  })
+  test('1990', () => {
+    expect(changeToRomanNumerals(1990)).toBe('MCMXC');
+  })
+  test('2007', () => {
+    expect(changeToRomanNumerals(2007)).toBe('MMVII');
+  })
+  test('2008', () => {
+    expect(changeToRomanNumerals(2008)).toBe('MMVIII');
+  })
+  test('2546', () => {
+    expect(changeToRomanNumerals(2546)).toBe('MMDXLVI');
+  })
+})
